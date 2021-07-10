e3ff83b9e34b0ccafa9e0756d049b62c0f40c910
shortcut2yp <yashpatel001@gmail.com>
2021-07-10 14:27:43 +0100

    changing the backend server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'font-awesome/css/font-awesome.css'
 import Main from './components/Main'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:4000')
+const socket = io('https://git.heroku.com/quick-chat999.git')
 
 function App() {
 	return <Main socket={socket} />
